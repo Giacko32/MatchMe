383f823c261dde884ca33dbde702d8862c61d05d
Giacko32 <giacomolombardo67@gmail.com>
2024-04-13 13:17:15 +0200

    il pulsante registrati adesso funziona

--- src/main/java/com/prova/matchme/MatchMeController.java
+++ src/main/java/com/prova/matchme/MatchMeController.java
@@ -2,13 +2,27 @@ package com.prova.matchme;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MatchMeController {
 
     @FXML
-    protected void registrazione()
-    {
-        FXMLLoader loader = new FXMLLoader(ApplicationMatchMe.class.getResource("Register-view.fxml"));
+    Button registerbutton;
+
+    @FXML
+    protected void registrazione() throws IOException {
+        Stage stage = (Stage) registerbutton.getScene().getWindow();
+        if(getClass().getResource("Register-view.fxml") != null) {
+            Parent root = FXMLLoader.load(getClass().getResource("Register-view.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
 }
