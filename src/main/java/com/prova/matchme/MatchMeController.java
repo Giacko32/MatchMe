bb5dcdd480cde6ba0efc3aa16cf65e55b3402882
Giacko32 <giacomolombardo67@gmail.com>
2024-04-23 00:00:34 +0200

    sas

--- src/main/java/com/prova/matchme/MatchMeController.java
+++ src/main/java/com/prova/matchme/MatchMeController.java
@@ -2,26 +2,31 @@ package com.prova.matchme;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MatchMeController {
+public class MatchMeController{
 
     @FXML
-    Button registerbutton;
+    public Button registerbutton;
 
     @FXML
-    protected void registrazione() throws IOException {
+    public void toRegister() {
+        FXMLLoader loader = new FXMLLoader(ApplicationMatchMe.class.getResource("auth/Register-view.fxml"));
         Stage stage = (Stage) registerbutton.getScene().getWindow();
-        if(getClass().getResource("auth/Register-view.fxml") != null) {
-            Parent root = FXMLLoader.load(getClass().getResource("auth/Register-view.fxml"));
-            Scene scene = new Scene(root);
+        try {
+            Scene scene = new Scene(loader.load());
             stage.setScene(scene);
             stage.show();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
         }
     }
 
